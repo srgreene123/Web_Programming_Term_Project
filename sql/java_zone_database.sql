9a84a9377aa235ccd36c756bbb0af6fe6caaae1e
Sarah Greene <72089327+srgreene123@users.noreply.github.com>
2021-12-03 17:59:11 -0500

    Removed unnecessary primary key from cart table

--- sql/java_zone_database.sql
+++ sql/java_zone_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 03, 2021 at 11:49 PM
+-- Generation Time: Dec 03, 2021 at 11:57 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.27
 
@@ -120,7 +120,7 @@ INSERT INTO `users` (`userID`, `firstName`, `lastName`, `username`, `password`, 
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
-  ADD PRIMARY KEY (`cartID`,`orderID`);
+  ADD PRIMARY KEY (`cartID`);
 
 --
 -- Indexes for table `order_summary`
