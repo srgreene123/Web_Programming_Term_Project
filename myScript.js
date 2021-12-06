ba76e9fb2e963d2cf933ba0f6b81d35107cdd9d9
ADHUGA <91347296+ADHUGA@users.noreply.github.com>
2021-12-05 19:31:08 -0500

    Add files via upload
    Validated registration and checkout through html pattern. Show password with JavaScript. Card Number and CVV and Zipcode validation. Prevented user from submitting blank fields for registering.

---
+++ myScript.js
@@ -0,0 +1,25 @@
+function myFunction() 
+	{
+	  var x = document.getElementById("password");
+	  if (x.type === "password") 
+	  {
+		x.type = "text";
+	  } 
+	  else 
+	  {
+		x.type = "password";
+	  }
+	}
+	
+function myFunction2() 
+	{
+	  var x = document.getElementById("pswd");
+	  if (x.type === "password") 
+	  {
+		x.type = "text";
+	  } 
+	  else 
+	  {
+		x.type = "password";
+	  }
+	}
